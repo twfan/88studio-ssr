83b9b4ea00edefd7dc4e6c8611d9ab53a2c83f55
taufan <taufan.erlangga@gmail.com>
2024-03-21 15:25:59 +0700

    discount progress

--- resources/views/admin/discounts/create.blade.php
+++ resources/views/admin/discounts/create.blade.php
@@ -21,6 +21,26 @@
                                 <label class="text-sm text-slate-400">Amount</label>
                                 <input placeholder="Discount Amount" name="price" type="number" min="0" class="text-sm border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md" />
                             </div>
+                            <div class="flex flex-col w-96">
+                                <label class="text-sm text-slate-400">Use for</label>
+                                <div class="flex gap-3">
+                                    <div class="flex items-center text-sm gap-1">
+                                        <input type="radio" id="singleRadio" name="limitation" value="single" checked>
+                                        <label for="singleRadio">Single</label>
+                                    </div>
+                                    <div class="flex items-center text-sm gap-1">
+                                        <input type="radio" id="multipleRadio" name="limitation" value="multiple" checked>
+                                        <label for="multipleRadio">Multiple</label>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="flex flex-col w-96">
+                                <label class="text-sm text-slate-400" for="distributeTo">Distribute to</label>
+                                <select id="distributeTo" name="distribute_to" class="text-sm rounded block w-full p-2.5 border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md">
+                                    <option value="1" class="capitalize" selected disabled>Select</option>
+                                    <option value="all_user" class="capitalize">Everyone</option>
+                                </select>
+                            </div>
                             <div class="flex items-center justify-end mt-4">
                                 <a href="{{ route('admin.products.index') }}" class="text-sm underline">Cancel</a>
                                 <x-secondary-button class="ms-4" type="submit">
