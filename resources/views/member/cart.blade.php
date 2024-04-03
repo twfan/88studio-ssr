617d8c4394890c61c4ab34959f0436fc5a6b42c3
taufan <taufan.erlangga@gmail.com>
2024-04-04 00:07:07 +0700

    fix

--- resources/views/member/cart.blade.php
+++ resources/views/member/cart.blade.php
@@ -39,117 +39,120 @@
               @endforelse
               <tr>
                 <td colspan="3" class="text-center text-sm text-slate-300">
-                    <a href="{{route('ych-comission','static')}}" class="text-sm underline">Continue shopping for items</a>
+                    <a href="{{route('ych-comission')}}" class="text-sm underline">Continue shopping for items</a>
                 </td>
               </tr>
             </tbody>
           </table>
         </div>
+        @if ($carts->count() > 0)
         <div class="w-1/3 flex flex-col">
-            <div class="overflow-auto w-full min-h-1/2-screen min-h-lvh max-h-lvh flex flex-col justify-between mx-3 bg-white rounded border">
-                <div class="flex flex-col">
-                    <div class="p-5 flex flex-col">
-                        <h1 class="text-2xl mb-2">Commission Request</h1>
+          <div class="overflow-auto w-full min-h-1/2-screen min-h-lvh max-h-lvh flex flex-col justify-between mx-3 bg-white rounded border">
+              <div class="flex flex-col">
+                  <div class="p-5 flex flex-col">
+                      <h1 class="text-2xl mb-2">Commission Request</h1>
 
-                        <div class="bg-slate-100 p-2 rounded">
-                          <p>Requesting as <span class="capitalize">{{$user->name}}</span></p>
-                        </div>
-                    </div>
-                    <hr /> 
-                    <div class="flex flex-col mt-3 px-5 py-3 gap-7">
-                        <div class=" mb-3">
-                          <p>Once you submit your request, we'll review it to determine if we're the right fit for your needs. If so, we'll send you a proposal with your exact pricing and timing before we move forward. Please provide as much detail upfront as possible, thankyou!</p>
-                        </div>
-                        <form action="{{route('member.cart.checkout')}}" method="POST" enctype="multipart/form-data">
-                          @csrf
-                          <ol class="flex flex-col gap-10 p-3 list-decimal">
-                            <li>
+                      <div class="bg-slate-100 p-2 rounded">
+                        <p>Requesting as <span class="capitalize">{{$user->name}}</span></p>
+                      </div>
+                  </div>
+                  <hr /> 
+                  <div class="flex flex-col mt-3 px-5 py-3 gap-7">
+                      <div class=" mb-3">
+                        <p>Once you submit your request, we'll review it to determine if we're the right fit for your needs. If so, we'll send you a proposal with your exact pricing and timing before we move forward. Please provide as much detail upfront as possible, thankyou!</p>
+                      </div>
+                      <form action="{{route('member.cart.checkout')}}" method="POST" enctype="multipart/form-data">
+                        @csrf
+                        <ol class="flex flex-col gap-10 p-3 list-decimal">
+                          <li>
+                            <div class="flex flex-col gap-1">
+                              <p>Please select the discount that you want to use.</p>
+                              <select class="rounded-2xl" name="discount" id="discounts">
+                                <option value="select" disabled selected>Select discount</option>
+                                @foreach ($discounts as $item)
+                                    <option value="{{$item->id}}">{{$item->name}}</option>
+                                @endforeach
+                              </select>
+                            </div>
+                          </li>
+                          <li>
+                            <div class="flex flex-col gap-1 ">
+                              <p>Please provide any social media or any platform that you use (especially where we can contact you)</p>
+                              <textarea name="socialMedia" class="w-full rounded text-sm border border-gray-200" placeholder="Social media" rows="5" required></textarea>
+                            </div>
+                          </li>
+                          <li>
+                            <div class="flex-col gap-1">
+                              <p>How will you be using this work</p>
                               <div class="flex flex-col gap-1">
-                                <p>Please select the discount that you want to use.</p>
-                                <select class="rounded-2xl" name="discount" id="discounts">
-                                  <option value="select" disabled selected>Select discount</option>
-                                  @foreach ($discounts as $item)
-                                      <option value="{{$item->id}}">{{$item->name}}</option>
-                                  @endforeach
-                                </select>
+                                  <div class="flex items-center gap-1">
+                                    <input type="radio" id="personal" name="useFor" value="personal" required>
+                                    <label for="personal">Personal</label>
+                                  </div>
+                                  <div class="flex items-center gap-1">
+                                    <input type="radio" id="streaming" name="useFor" value="streaming">
+                                    <label for="streaming">Commercial/streaming</label>
+                                  </div>
+                                  <div class="flex items-center gap-1">
+                                    <input type="radio" id="merchandise" name="useFor" value="merchandise">
+                                    <label for="merchandise">Commercial/merchandise</label>
+                                  </div>
+                                  <div class="flex items-center gap-1">
+                                    <input type="radio" id="other" name="useFor" value="other">
+                                    <input type="text" name="useForOther" class="border-slate-200 px-3 py-2 w-full" placeholder="Other">
+                                  </div>
                               </div>
-                            </li>
-                            <li>
-                              <div class="flex flex-col gap-1 ">
-                                <p>Please provide any social media or any platform that you use (especially where we can contact you)</p>
-                                <textarea name="socialMedia" class="w-full rounded text-sm border border-gray-200" placeholder="Social media" rows="5" required></textarea>
+                            </div>
+                          </li>
+                          <li>
+                            <div class="flex flex-col gap-1">
+                              <p>Please provide reference for your character and the emotes you need (preferably front view with proper lighting if there's any)</p>
+                              <input type="file" name="refferences" required>
+                            </div>
+                          </li>
+                          <li>
+                            <div class="flex flex-col">
+                              <p>Please specify your hard deadline if there's any</p>
+                              <div class="bg-gray-100 rounded-2xl p-3">
+                                  <div class="bg-gray-100 rounded-2xl">
+                                    <input name="deadline" class="w-full bg-gray-100 border-transparent focus:border-transparent" type="text" id="datepicker" autocomplete="off">
+                                  </div>
                               </div>
-                            </li>
-                            <li>
-                              <div class="flex-col gap-1">
-                                <p>How will you be using this work</p>
-                                <div class="flex flex-col gap-1">
-                                    <div class="flex items-center gap-1">
-                                      <input type="radio" id="personal" name="useFor" value="personal" required>
-                                      <label for="personal">Personal</label>
-                                    </div>
-                                    <div class="flex items-center gap-1">
-                                      <input type="radio" id="streaming" name="useFor" value="streaming">
-                                      <label for="streaming">Commercial/streaming</label>
-                                    </div>
-                                    <div class="flex items-center gap-1">
-                                      <input type="radio" id="merchandise" name="useFor" value="merchandise">
-                                      <label for="merchandise">Commercial/merchandise</label>
-                                    </div>
-                                    <div class="flex items-center gap-1">
-                                      <input type="radio" id="other" name="useFor" value="other">
-                                      <input type="text" name="useForOther" class="border-slate-200 px-3 py-2 w-full" placeholder="Other">
-                                    </div>
-                                </div>
-                              </div>
-                            </li>
-                            <li>
-                              <div class="flex flex-col gap-1">
-                                <p>Please provide reference for your character and the emotes you need (preferably front view with proper lighting if there's any)</p>
-                                <input type="file" name="refferences" required>
-                              </div>
-                            </li>
-                            <li>
-                              <div class="flex flex-col">
-                                <p>Please specify your hard deadline if there's any</p>
-                                <div class="bg-gray-100 rounded-2xl p-3">
-                                    <div class="bg-gray-100 rounded-2xl">
-                                      <input name="deadline" class="w-full bg-gray-100 border-transparent focus:border-transparent" type="text" id="datepicker" autocomplete="off">
-                                    </div>
-                                </div>
-                              </div>
-                            </li>
-                            <li>
-                              <div class="flex flex-col">
-                                <p>Please send us proof of payment after you have paid</p>
-                              </div>
-                            </li>
-                            <li>
-                              <div class="flex flex-col gap-1">
-                                <p>If you have ordered from us before and you want to use the same character for your request please send a screenshot of that previous work</p>
-                                <input type="file" name="latestDesign">
-                              </div>
-                            </li>
-                            {{-- <li>
-                              <div class="flex flex-col gap-1">
-                                <label for="cars">Feel free to choose a promotional voucher in case you possess one.</label>
-                                <select class="rounded-2xl" name="cars" id="cars">
-                                  <option value="volvo">Volvo</option>
-                                  <option value="saab">Saab</option>
-                                  <option value="mercedes">Mercedes</option>
-                                  <option value="audi">Audi</option>
-                                </select>
-                              </div>
-                            </li> --}}
-                          </ol>
-                          <button type="submit" class="py-3 mt-3 w-full rounded bg-88-orange text-white uppercase">
-                              Submit
-                          </button>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
+                            </div>
+                          </li>
+                          <li>
+                            <div class="flex flex-col">
+                              <p>Please send us proof of payment after you have paid</p>
+                            </div>
+                          </li>
+                          <li>
+                            <div class="flex flex-col gap-1">
+                              <p>If you have ordered from us before and you want to use the same character for your request please send a screenshot of that previous work</p>
+                              <input type="file" name="latestDesign">
+                            </div>
+                          </li>
+                          {{-- <li>
+                            <div class="flex flex-col gap-1">
+                              <label for="cars">Feel free to choose a promotional voucher in case you possess one.</label>
+                              <select class="rounded-2xl" name="cars" id="cars">
+                                <option value="volvo">Volvo</option>
+                                <option value="saab">Saab</option>
+                                <option value="mercedes">Mercedes</option>
+                                <option value="audi">Audi</option>
+                              </select>
+                            </div>
+                          </li> --}}
+                        </ol>
+                        <button type="submit" class="py-3 mt-3 w-full rounded bg-88-orange text-white uppercase">
+                            Submit
+                        </button>
+                      </form>
+                  </div>
+              </div>
+          </div>
+      </div>
+        @endif
+        
     </div>
 </x-member-layout>
 
