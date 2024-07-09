aa1633c5d00a9e91898236c121328d577f3ca453
taufan <taufan.erlangga@gmail.com>
2024-07-09 14:51:20 +0700

    fix footer and header

--- resources/views/components/sign-up-disc.blade.php
+++ resources/views/components/sign-up-disc.blade.php
@@ -1,8 +1,14 @@
 <div class="flex items-center w-full bg-black h-20 text-white text-2xl text-center justify-center">
-    <a href="{{route('member.login')}}">
-        <div class="flex font-['Lilita_One']">
-            <span>sign up now</span>
-            <button class="ml-2 border-2 border-solid border-yellow-400 rounded-full px-3">50% off</button>
-        </div> 
-    </a>
+    @if(auth()->check()) 
+    <div class="flex font-['Lilita_One'] uppercase">
+        <span>Hi {{auth()->user()->name}}, <span class="text-88-orange">How's your day</span></span>
+    </div> 
+    @else
+        <a href="{{route('member.login')}}">
+            <div class="flex font-['Lilita_One']">
+                <span>sign up now</span>
+                <button class="ml-2 border-2 border-solid border-yellow-400 rounded-full px-3">50% off</button>
+            </div> 
+        </a>
+    @endif
 </div>
