68eaf784e3080cd6e77d4a3668ca99e04493757a
taufan <taufan.erlangga@gmail.com>
2024-07-15 21:07:54 +0700

    fix

--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -5,7 +5,10 @@
             <x-front-menu :user="$user ?? null"></x-front-menu>
             <div class="w-full h-screen bg-welcome bg-no-repeat bg-center bg-cover flex flex-col justify-center items-center content-center">
                 <div class="flex flex-col">
-                    <div class="flex flex-col text-left">
+                    <div class="flex flex-col text-center items-center">
+                        <div class="w-1/3 h-1/3">
+                            <img src="{{ asset('images/shop-asset.webp') }}" alt="">
+                        </div>
                         <h1 class="text-6xl uppercase text-white mb-2 font-['Lilita_One']">Coming Soon</h1> 
                 </div>
             </div>
